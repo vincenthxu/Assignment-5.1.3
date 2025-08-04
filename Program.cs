719bcf79f15ef47006c7531bd5c3a5dc315d6ab6
Vincent Xu <vincent.xu1@outlook.com>
2025-08-04 12:57:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment_5._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                int[] ints = new int[args.Length];
+                for(int i = 0; i < args.Length; i++)
+                {
+                    ints[i] = Convert.ToInt32(args[i]);
+                }
+                Console.WriteLine(IsIndividuallyUnique(ints));
+            }
+        }
+        static bool IsIndividuallyUnique(int[] ints)
+        {
+            HashSet<int> elements = new();
+
+            foreach (int i in ints)
+            {
+                if(elements.Contains(i))
+                {
+                    return false;
+                }
+                else
+                {
+                    elements.Add(i);
+                }
+            }
+
+            return true;
+        }
+    }
+}
